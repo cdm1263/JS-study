73f74cf2284f0858e534733141811195a3285976
cdm1263 <122417731+cdm1263@users.noreply.github.com>
2023-03-14 10:22:53 +0900

    JS-study 20230314
    todo update

--- js/todo.js
+++ js/todo.js
@@ -4,7 +4,7 @@ const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
 
-const toDos = [];
+let toDos = [];
 
 function saveToDos() {
     localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
@@ -18,8 +18,10 @@ function deleteToDo (event) {
 
 function painToDo(newTodo) {
     const li = document.createElement("li")
+    // 아래 newTodoObj에서 할당한 id를 li태그에 id속성값으로 붙여줌
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌"
     button.addEventListener("click", deleteToDo);
@@ -34,8 +36,13 @@ function handleToDoSubmit(event) {
     const newTodo = toDoInput.value;
     // 그 후에 인풋값을 비워도 newTodo에는 이전에 저장된 값이 그대로 있음
     toDoInput.value = "";
-    toDos.push(newTodo);
-    painToDo(newTodo);
+    // string형태로 주던 newTodo 대신 object형식으로 만들어 각 내용이 고유한 랜덤id를 갖는 newTodoObj를 만듬
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now()
+    }
+    toDos.push(newTodoObj);
+    painToDo(newTodoObj);
     saveToDos();
 }
 
@@ -50,5 +57,7 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if(savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos);
-    parsedToDos.forEach(element => console.log("this is the turn of", element));
+    // 새로고침 할 때마다 toDos의 빈 array를 반환하기 때문에 기존에 입력했던 array를 toDos에 덮어씌움
+    toDos = parsedToDos;
+    parsedToDos.forEach(painToDo);
 }
